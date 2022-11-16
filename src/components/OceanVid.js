6eb3f2bd1ac2fa16a087c8867515a2f0fa326b08
Solvita <solvita.dvortsova@henleyglobal.com>
2022-11-16 18:10:30 +0200

    hooks and vid

--- src/components/OceanVid.js
+++ src/components/OceanVid.js
@@ -1,12 +1,19 @@
 import MeditationGuide from "./MeditationGuide";
+import { useWindowDimensions } from "../hooks/WindowSize.js";
 
 function OceanVideo() {
+  const { width } = useWindowDimensions();
+
   return (
     <>
       <div className="video-wrapper">
-        <video autoPlay muted loop>
-          <source src="ocean.mp4" type="video/mp4" />
-        </video>
+        {width > 767 ? (
+          <video autoPlay muted loop>
+            <source src="ocean.mp4" type="video/mp4" />
+          </video>
+        ) : (
+          ""
+        )}
       </div>
       <MeditationGuide />
     </>
